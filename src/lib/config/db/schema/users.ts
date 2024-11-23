b7ae79d5f29ae9dfaf517d1b42340d6c8a5c66b3
TomoorIrorI <Ytomohiro.sasuke@gmail.com>
2024-11-23 20:50:33 +0900

    Update schema of users

--- src/lib/config/db/schema/users.ts
+++ src/lib/config/db/schema/users.ts
@@ -1,12 +1,12 @@
 //lib/config/db/schema/users.ts
 
 //usersテーブルの定義
-import { pgTable, integer, serial, varchar, timestamp } from "drizzle-orm/pg-core"
+import { pgTable, serial, varchar, timestamp, uuid } from "drizzle-orm/pg-core"
 
-export const users = pgTable('users', {
-    tableId: serial().notNull().primaryKey(),
-    uuid: integer().generatedAlwaysAsIdentity().notNull().unique(),
-    email: varchar({ length: 255 }).notNull().unique(),
-    password: varchar({ length: 30}).notNull(),
-    registered_at: timestamp().notNull()
+export const users = pgTable("users", {
+    tableId: serial("table_id").notNull().primaryKey(),
+    uuid: uuid("uuid").defaultRandom().notNull().unique(), // UUID 型で一意の ID
+    email: varchar("email", { length: 255 }).notNull().unique(), // メールアドレス
+    password: varchar("password", { length: 255 }).notNull(), // ハッシュ化されたパスワード
+    registeredAt: timestamp("registered_at").defaultNow().notNull(), // 登録日時
 });
