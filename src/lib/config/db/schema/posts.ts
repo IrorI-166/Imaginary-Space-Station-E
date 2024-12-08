eec3f2f66e4b41aefc530bc3c4115d137ea2e8c3
TomoorIrorI <Ytomohiro.sasuke@gmail.com>
2024-12-08 15:40:44 +0900

    Update

--- src/lib/config/db/schema/posts.ts
+++ src/lib/config/db/schema/posts.ts
@@ -2,8 +2,11 @@
 
 //Post情報テーブルの定義
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { get } from "http";
+import { users } from "./users";
 
 export const posts = pgTable('posts', {
-    postId: serial().notNull().primaryKey(),
+    postId: serial().notNull().primaryKey().generatedAlwaysAsIdentity(),
+    authorId: get(users.uuid).notNull(),
     content: text().notNull()
 });
